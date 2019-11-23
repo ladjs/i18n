ae85c4334299cc5b959c15b55cd67870e5a1ca2b
niftylettuce <niftylettuce@gmail.com>
2019-11-22 18:03:46 -0600

    fix: set autoReload to true by default (causes watcher to reload)

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ class I18N {
         indent: '  ',
         defaultLocale: 'en',
         syncFiles: boolean(process.env.I18N_SYNC_FILES || true),
-        autoReload: boolean(process.env.I18N_AUTO_RELOAD || false),
+        autoReload: boolean(process.env.I18N_AUTO_RELOAD || true),
         updateFiles: boolean(process.env.I18N_UPDATE_FILES || true),
         api: {
           __: 't',
